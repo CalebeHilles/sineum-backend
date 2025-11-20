4bade7fc58f3b8c2dc39eefbd07f35d940b64907
Kleb-0 <shadowhilles@gmail.com>
2025-11-20 18:11:59 -0300

    refactor: fix bugs and improve error handling in DeleteBlogs controller
    - Fix: use single Blog instance instead of array
    - Fix: verify if blog exists before deleting
    - Add proper 500 status code and error message

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -53,8 +53,21 @@ func DeleteBlogs(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	var b []models.Blog
-	database.DB.Delete(&b, id)
+	var b models.Blog
+	fetchResult := database.DB.First(&b, id)
+	
+	if fetchResult.Error != nil {
+		http.Error(w, "Blog not found", http.StatusNotFound)
+		return
+	}
+	
+	result := database.DB.Delete(&b, id)
+	
+	if result.Error != nil {
+		http.Error(w, "Server failed deleting this blog", http.StatusInternalServerError)
+		return
+	}
+
 	json.NewEncoder(w).Encode(b)
 }
 
